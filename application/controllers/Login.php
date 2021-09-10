9a139760504e74c8f399ff78b8d4abbfe4900f97
Bagus <sgiapi1@gmail.com>
2021-09-10 17:15:11 +0700

    Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,10 +4,21 @@ class Login extends CI_Controller
 {
 	public function index()
 	{
-		$data['judul'] = 'Login';
-		$this->load->view('templates/header', $data);
-		$this->load->view('login/index');
-		$this->load->view('templates/footer');
+		if ($this->session->userdata('email')) {
+			redirect('dashboard');
+		}
+
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[12]', ['min_length' => 'Password too short (Min. 12 character)']);
+
+		if ($this->form_validation->run() == false) {
+			$data['judul'] = 'Login';
+			$this->load->view('templates/header', $data);
+			$this->load->view('login/index');
+			$this->load->view('templates/footer');
+		} else {
+			$this->_login();
+		}
 	}
 
 	public function registration()
@@ -63,5 +74,40 @@ class Login extends CI_Controller
 		}
 
 		redirect('login');
+	}
+
+	private function _login()
+	{
+		$url = 'http://localhost:5000/signin';
+		/* Init cURL resource */
+		$ch = curl_init($url);
+		$data = [
+			"email" => $this->input->post('email'),
+			"password" => $this->input->post('password'),
+		];
+
+		/* pass encoded JSON string to the POST fields */
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+		/* set the content type json */
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+		/* set return type json */
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		/* execute request */
+		curl_exec($ch);
+
+		/* close cURL resource */
+		// curl_close($ch);
+
+		$responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		if ($responseCode == 422) {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Invalid Email or password !</div>');
+			redirect('login');
+		}
+
+		redirect('dashboard');
 	}
 }
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -3,15 +3,25 @@
 		<div class="col-sm-3">
 		</div>
 		<div class="col-sm-6">
-			<form>
+			<form method="post" action="/login">
+				<div class="msg align-center">
+					<h1 class="title text-center">Login</h1>
+				</div><br>
+				<?php if ($this->session->flashdata('message')) { ?>
+					<div class="">
+						<?= $this->session->flashdata('message') ?>
+					</div>
+				<?php
+				} ?>
 				<div class="form-group">
 					<label for="exampleInputEmail1">Email address</label>
 					<input type="email" class="form-control" name="email" placeholder="Enter email">
-					<!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
+					<?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
 				</div>
 				<div class="form-group">
 					<label for="exampleInputPassword1">Password</label>
 					<input type="password" class="form-control" name="password" placeholder="Password">
+					<?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
 				</div>
 				<div class="row">
 					<div class="col-sm-12">
